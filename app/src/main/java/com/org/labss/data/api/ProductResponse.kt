766794d66ea0463420636e439015929d84728a72
DmytroDubov <249496071+DmytroDubov@users.noreply.github.com>
2026-03-11 14:47:15 +0200

    Revert "Fix/screen rotation"

--- app/src/main/java/com/org/labss/data/api/ProductResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.org.labss.data.api
-
-data class ProductResponse(
-    val record: List<ProductDto>
-)
--- app/src/main/java/com/org/labss/ui/theme/Theme.kt
+++ app/src/main/java/com/org/labss/ui/theme/Theme.kt
@@ -1,26 +1,58 @@
 package com.org.labss.ui.theme
 
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import com.org.labss.ui.theme.BlackPrimary
-import com.org.labss.ui.theme.LightGraySurface
-import com.org.labss.ui.theme.White
+import androidx.compose.ui.platform.LocalContext
 
-private val AppColorScheme = lightColorScheme(
-    background = White,
-    surface = LightGraySurface,
-    primary = BlackPrimary,
-    onPrimary = White,
-    onBackground = BlackPrimary,
-    onSurface = BlackPrimary
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
-fun LabssTheme(content: @Composable () -> Unit) {
+fun LabssTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
     MaterialTheme(
-        colorScheme = AppColorScheme,
-        typography = AppTypography,
+        colorScheme = colorScheme,
+        typography = Typography,
         content = content
     )
 }
