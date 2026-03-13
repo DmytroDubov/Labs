240bea7cd130d98175f1e4df9659397baaf93c1a
DMYTRO DUBOV <dubovdimka@gmail.com>
2026-03-13 12:46:49 +0200

    updated ui v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.org.labss"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
